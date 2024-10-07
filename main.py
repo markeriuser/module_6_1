402c729f059f56502c4e7047a4d8450cd9cc5bde
Mark Sevi <mr.lms29@mail.ru>
2024-10-08 01:26:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,41 +4,32 @@ class Animal:
         self.fed = False
         self.name = name
 
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 
 class Plant:
     def __init__(self, name):
         self.edible = False
         self.name = name
 
-
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
-
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
-
+    pass
 
 class Flower(Plant):
     pass
 
-
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
-        self.edible = True
+        self.edible = True 
 
 
 a1 = Predator('Волк с Уолл-Стрит')
@@ -46,12 +37,15 @@ a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
+
 print(a1.name)
 print(p1.name)
 
 print(a1.alive)
 print(a2.fed)
+
 a1.eat(p1)
 a2.eat(p2)
+
 print(a1.alive)
 print(a2.fed)
